c95c54e73f8502e3113dfd53cd4495b44cfa3975
andreaskapou <kapouranis.andreas@gmail.com>
2015-02-12 16:14:55 +0000

    Script to read and generate data

---
+++ readData.R
@@ -0,0 +1,46 @@
+##=============================================
+# Script that will be called to read the data #
+##============================================= 
+
+##=============================================
+# Generate data:  mixture of 1D Gaussians     #
+##=============================================
+gen.gaussian <- function(N=500, K=2, pi=c(0.6,0.4), mus=c(10,20), stds=c(2,3)){
+  # Sample the N components according to their mixing proportions
+  components <- sample(1:K, prob=pi, size=N, replace=TRUE)
+  # Generate N random samples from a Gaussian, with the corresponding weights
+  samples    <- rnorm(n=N,mean=mus[components],sd=stds[components])
+  
+  #plot(density(samples),main="Density Estimate of the Mixture Model")
+  return (samples)
+}
+
+##=============================================
+# Generate data: mixture of 1D Poissons       #
+##=============================================
+gen.poisson <- function(N=500, K=2, pi=c(0.6,0.4), lambdas=c(8,22)){
+  # Sample the N components according to their mixing proportions
+  components <- sample(1:K, prob=pi, size=N, replace=TRUE)
+  # Generate N random samples from a Gaussian, with the corresponding weights
+  samples    <- rpois(n=N,lambda=lambdas[components])
+  
+  return (samples)
+}
+
+##============================================
+# Generate data: mixture of 1D Binomials     #
+##============================================
+gen.binomial <- function(N=500, K=2, pi=c(0.6,0.4), p=c(0.3,0.8), r){
+  # Sample the N components according to their mixing proportions
+  components <- sample(1:K, prob=pi, size=N, replace=TRUE)
+  # Generate N random samples from a Gaussian, with the corresponding weights
+  samples    <- rbinom(n=N, size=r, prob=p[components])
+  return (samples)
+}
+
+##=====================================
+# Read data from Old Faithful Dataset #
+##=====================================
+read.faithful1D <- function(dim=2){
+  X <-  faithful[,dim]
+}
