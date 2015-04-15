d5cf040e1b9f8c8311540137857919cccf92ece4
andreaskapou <kapouranis.andreas@gmail.com>
2015-04-15 12:46:15 +0100

    Minor bug in c.n.update function, changes in the whole function to make it more readable

--- Gibbs-Sampling/gmmMV-gibbs.R
+++ Gibbs-Sampling/gmmMV-gibbs.R
@@ -5,33 +5,32 @@ gmmMV.gibbs <-  function(X, K=2, N.Sims, burnin, Normal, pi.cur, dir.a, logl){
   # Normal-Gamma priors on mu and Tau, using multi-dimensional dataset  #
   ##=====================================================================
   
-  N           <- NROW(X)        # Number of rows (observations)
-  D           <- NCOL(X)        # Number of columns (variables - dimensions)       
-  pdf.w       <- matrix(, N, K) # PDF of each point on each cluster k
-  post.resp   <- matrix(, N, K) # Matrix for the posterior responsibilites
-  x.k.bar     <- matrix(, D, K) # Sample mean vector for each cluster k
-  if (is.vector(Normal$Tau[[1]])) # If diagonal covariance matrix
-    ssd.k     <- matrix(, D, K) # Sum of square differences on each cluster k
-  else
-    ssd.k     <- list()         # Sum of square differences on each cluster k
+  N         <- NROW(X)  # Number of rows (observations)
+  D         <- NCOL(X)  # Number of columns (variables - dimensions)  
   
-  mu.draws    <- list()   # Mean vector of each Gaussian
-  Tau.draws   <- list()   # Precision matrix of each Gaussian
-  pi.draws    <- matrix(, N.Sims-burnin, K)   # Mixing Proportions
-  #CPost       <- matrix(, N, K)   # Mixture Components
-  
-  total.mu    <- matrix(0, D, K)  # Store the sum of posterior means
-  total.Tau   <- list()           # Store the sum of posterior precisions
-  for (k in 1:K){
-    total.Tau[[k]] <- matrix(0, ncol=D, nrow=D)
+  pdf.w     <- matrix(0, nrow=N, ncol=K)  # PDF of each point on each cluster k
+  post.resp <- matrix(0, nrow=N, ncol=K)  # Matrix for the posterior responsibilites
+  x.k.bar   <- matrix(0, nrow=K, ncol=D)  # Sample mean vector for each cluster k
+  if (is.vector(Normal$Tau[[1]])){        # If diagonal covariance matrix
+    ssd.k   <- matrix(0, nrow=K, ncol=D)  # Sum of square differences on cluster k
+  }else{
+    ssd.k   <- list()     # Sum of square differences on each cluster k
   }
+  total.mu  <- matrix(0, nrow=K, ncol=D)  # Store the sum of posterior means
+  total.Tau <- list()                     # Store the sum of posterior precisions
+  for (k in 1:K){
+    total.Tau[[k]] <- matrix(0, nrow=D, ncol=D)
+  }
+  pi.draws  <- matrix(0, nrow=N.Sims-burnin, ncol=K) # Mixing Proportions
+  mu.draws  <- list()     # Mean vector of each Gaussian
+  Tau.draws <- list()     # Precision matrix of each Gaussian
   
-  #all(m[!diag(nrow(m))] == 0) 
-  for (t in 1:N.Sims){
+  
+  for (t in 1:N.Sims){    # Start Gibbs sampling
     # Compute responsibilities
     post.resp   <- compute.resp(X, pdf.w, K, Normal, pi.cur, logl)
     # Draw mixture components for ith simulation
-    C.n         <- c.n.update(N, post.resp)
+    C.n         <- c.n.update(N, K, post.resp)
     # Calculate component counts of each cluster
     N.k         <- colSums(C.n)
     # Update mixing proportions using new cluster component counts
@@ -39,29 +38,29 @@ gmmMV.gibbs <-  function(X, K=2, N.Sims, burnin, Normal, pi.cur, dir.a, logl){
     
     for (k in 1:K){ # Calculate sample mean and SSD for each cluster
       x.k.bar[k,] <- colMeans(X[C.n[,k] == 1,])
-      # apply(X[C.n[,k]==1,], MARGIN=2, FUN='sd')^2 * N.k[k]
-      if (is.vector(Normal$Tau[[1]]))  # If diagonal covariance matrix
+                    # apply(X[C.n[,k]==1,], MARGIN=2, FUN='sd')^2 * N.k[k]
+      if (is.vector(Normal$Tau[[1]])){  # If diagonal covariance matrix
         ssd.k[k,]   <- rowSums(apply(X[C.n[,k] == 1,],1,'-',x.k.bar[k,])^2)
-      else
+      }else{
         ssd.k[[k]] <- apply(X[C.n[,k] == 1,],1,'-',x.k.bar[k,]) %*% 
                               t(apply(X[C.n[,k] == 1,],1,'-',x.k.bar[k,]))
+      }
     }
-    
     if (is.vector(Normal$Tau[[1]])){  # If diagonal covariance matrix
       # Update posterior precision 
-      Tau.cur <- tau.update.diag(K, Normal, N.k, x.k.bar, ssd.k)
+      Tau.cur <- tau.update.diag(K, D, Normal, N.k, x.k.bar, ssd.k)
       # Update posterior mean
-      mu.cur  <- mu.update.diag(K, Normal, N.k, x.k.bar)
+      mu.cur  <- mu.update.diag(K, D, Normal, N.k, x.k.bar)
     }else{
       # Update posterior precision
-      Tau.cur <- tau.update.full(K, Normal, N.k, x.k.bar, ssd.k)
+      Tau.cur <- tau.update.full(K, D, Normal, N.k, x.k.bar, ssd.k)
       # Update posterior mean
-      mu.cur  <- mu.update.full(K, Normal, N.k, x.k.bar)
+      mu.cur  <- mu.update.full(K, D, Normal, N.k, x.k.bar)
     }
     
     # Keep only the simulations after the burn-in period has passed
     if (t > burnin){
-      total.mu          <- total.mu + mu.cur
+      total.mu  <- total.mu + mu.cur
       if (is.vector(Normal$Tau[[1]])){
         for (k in 1:K){
           total.Tau[[k]]  <- total.Tau[[k]] + diag(Tau.cur[k,])
@@ -71,9 +70,9 @@ gmmMV.gibbs <-  function(X, K=2, N.Sims, burnin, Normal, pi.cur, dir.a, logl){
           total.Tau[[k]]  <- total.Tau[[k]] + Tau.cur[[k]]
         }
       }
-      mu.draws[[t - burnin]]    <- mu.cur
-      Tau.draws[[t - burnin]]   <- Tau.cur
-      pi.draws[t - burnin,]     <- pi.cur
+      mu.draws[[t - burnin]]  <- mu.cur
+      Tau.draws[[t - burnin]] <- Tau.cur
+      pi.draws[t - burnin,]   <- pi.cur
     }
   }
   return(list(pi.draws=pi.draws, mu.draws=mu.draws, Tau.draws=Tau.draws,
@@ -109,8 +108,8 @@ compute.resp <- function(X, pdf.w, K, Normal, pi.cur, logl){
   return(post.resp)
 }
 # Update the mixture components
-c.n.update <- function(N, post.resp){
-  c.i.draw <- matrix(, N, K)
+c.n.update <- function(N, K, post.resp){
+  c.i.draw <- matrix(, nrow=N, ncol=K)
   for (i in 1:N)  # Sample one point from a multinomial i.e. ~ Discrete
     c.i.draw[i,] = rmultinom(1, 1, post.resp[i,])
   return(c.i.draw)
@@ -121,9 +120,8 @@ pi.update <- function(dir.a, N.k){
   return(as.vector(rdirichlet(n=1, alpha=a.n))) # Sample from Dirichlet
 }
 # Update the posterior precision diagonal covariance matrix
-tau.update.diag <- function(K, Normal, N.k, x.k.bar, ssd.k){
-  D <- NCOL(x.k.bar)
-  tau.posterior <- matrix(0, ncol=D, nrow=K)
+tau.update.diag <- function(K, D, Normal, N.k, x.k.bar, ssd.k){
+  tau.posterior <- matrix(0, nrow=K, ncol=D)
   for (k in 1:K){
     alpha.n <- Normal$Gamma$a + N.k[k]/2
     beta.n  <- Normal$Gamma$b + ssd.k[k,]/2 + ( 
@@ -134,8 +132,7 @@ tau.update.diag <- function(K, Normal, N.k, x.k.bar, ssd.k){
   return(tau.posterior)
 }
 # Update the posterior precision full covariance matrix
-tau.update.full <- function(K, Normal, N.k, x.k.bar, ssd.k){
-  D <- NCOL(x.k.bar)
+tau.update.full <- function(K, D, Normal, N.k, x.k.bar, ssd.k){
   tau.posterior <- list()
   for (k in 1:K){
     v.n   <- Normal$Wishart$v + N.k[k]
@@ -147,9 +144,8 @@ tau.update.full <- function(K, Normal, N.k, x.k.bar, ssd.k){
   return(tau.posterior)
 }
 # Update the posterior mean diagonal covariance matrix
-mu.update.diag <- function(K, Normal, N.k, x.k.bar){
-  D <- NCOL(x.k.bar)
-  mu.posterior <- matrix(0, ncol=D, nrow=K)
+mu.update.diag <- function(K, D, Normal, N.k, x.k.bar){
+  mu.posterior <- matrix(0, nrow=K, ncol=D)
   for (k in 1:K){
     t.n.k   <- Normal$Norm$tau.0 + N.k[k]
     mu.n    <- (Normal$Norm$mu.0*Normal$Norm$tau.0 + N.k[k]*x.k.bar[k,]) / t.n.k
@@ -159,9 +155,8 @@ mu.update.diag <- function(K, Normal, N.k, x.k.bar){
   return(mu.posterior)
 }
 # Update the posterior mean full covariance matrix
-mu.update.full <- function(K, Normal, N.k, x.k.bar){
-  D <- NCOL(x.k.bar)
-  mu.posterior <- matrix(0, ncol=D, nrow=K)
+mu.update.full <- function(K, D, Normal, N.k, x.k.bar){
+  mu.posterior <- matrix(0, nrow=K, ncol=D)
   for (k in 1:K){
     t.n.k   <- Normal$Wishart$k.0 + N.k[k]
     mu.n    <- (Normal$Norm$mu.0*Normal$Wishart$k.0+N.k[k]*x.k.bar[k,]) / t.n.k
