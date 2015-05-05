0a8036a07e771dfb020bb9382d80b8007271cb13
andreaskapou <kapouranis.andreas@gmail.com>
2015-05-05 21:03:19 +0100

    Almost finish the EM model for clustering methylation profile data. The M step needs to be implemented.

--- Expectation-Maximization/em-probit-reg.R
+++ Expectation-Maximization/em-probit-reg.R
@@ -8,79 +8,85 @@ setwd(cur.dir)
 library(R.utils)
 sourceDirectory("../lib", modifiedOnly=FALSE) # Source the 'lib' directory
 
+ff <- function(theta, X){
+  g   <- theta[1]*X^2 + theta[2]*X + theta[3]
+  g <- pnorm(g)
+  return(g)
+}
 
 
-fpr <- function(theta, n.i) {
-  a   <- theta[1]
-  b   <- theta[2]
-  c   <- theta[3]
-  m   <- n.i$m
-  t   <- n.i$t
-  x   <- m / t
-  g   <- a*x^2 + b*x + c
-  Phi <- pnorm(g) + 1e-10
-  res <- -dbinom(x=m, size=t, prob=Phi, log=TRUE)
-  res <- sum(res)
+fpr <- function(theta, D){
+  X   <- D[1]
+  t   <- D[2]
+  m   <- D[3]
+  g   <- theta[1]*X^2 + theta[2]*X + theta[3]
+  Phi <- pnorm(g)
+  res <- sum(-dbinom(x=m, size=t, prob=Phi, log=TRUE))
   return(res)
 }
 
-gpr <- function(theta, n.i){
-  a   <- theta[1]
-  b   <- theta[2]
-  c   <- theta[3]
-  m   <- n.i$m
-  t   <- n.i$t
-  x   <- m / t
-  g   <- a*x^2 + b*x + c
-  Phi <- pnorm(g) + 1e-10
+gpr <- function(theta, D){
+  X   <- D[1]
+  t   <- D[2]
+  m   <- D[3]
+  g   <- theta[1]*X^2 + theta[2]*X + theta[3]
+  Phi <- pnorm(g)
   N   <- dnorm(g)
   
-  da  <- sum(-N*x^2 * (m-t*Phi)/(Phi*(1-Phi)))
-  db  <- sum(-N*x * (m-t*Phi)/(Phi*(1-Phi)))
+  da  <- sum(-N*X^2 * (m-t*Phi)/(Phi*(1-Phi)))
+  db  <- sum(-N*X * (m-t*Phi)/(Phi*(1-Phi)))
   dc  <- sum(-N * (m-t*Phi)/(Phi*(1-Phi)))
   
   c(da, db, dc)
 }
 
 
+#t <- c(10, 11, 11, 11, 10, 10)
+#m <- c(8, 7, 7, 3, 2, 2)
+#X <- c(-50, -40, -30, 20, 30, 50)/(50/2)
+#ss <- optim(par=c(0,0,0), fn=fpr, gr=gpr, X, t, m, method="CG")
+
+
+# plot the function
+#xs <- seq(-1,1,len=100) # create some values
+#plot(x=xs, y=ff(theta=ss$par, xs), type="l", xlab="x", ylab="", 
+#     main=expression(a*x^2 + b*x + c))
+
 ##====================
 # Generate the data  #
 ##====================
 epsilon <- 0.00001    # Convergence paramater
-N       <- 200        # Total number of objects to create
+N       <- 300        # Total number of objects to create
 K       <- 3          # Number of clusters
-r1      <- rbinom(n=N, size=40, prob=.8)  # Total number of trials
-r       <- matrix(r1, ncol=1)
-X       <- gen.binomial(K=3, pi.c=c(.4,.3,.3), p=c(0.2,0.7, 0.5), r=r)
+pi.c    <- c(0.45, 0.35, 0.2) # Mixing proportions
+
+theta   <- matrix(0, nrow=K, ncol=3)
+for (i in 1:K){
+  theta[i,] <- c(0+i/100, 0+i/100, 0+i/100)
+}
+
+X       <- gen.meth.data(N=N, pi.c=pi.c) # Generate methylation profiles data
 
 pdf.w       <- matrix(, N, K) # Hold the PDF of each point on each cluster k
+post.resp   <- matrix(, N, K)
+log.likel   <- 0
 
-
-for (k in 1:K){ # Calculate the PDF of each cluster for each data point
-  pdf.w[,k] <- pi.c[k] * dbinom(X, size=r, prob=p[k])
-}
-post.resp   <- pdf.w / rowSums(pdf.w) # Get responsibilites by normalizarion
-
-for (i in 1:1000){
+for (t in 1:1000){
   ## E-step
-  for (k in 1:K) # Calculate the PDF of each cluster for each data point
-    pdf.w[,k] <- log(pi.c[k]) + fpr(theta, n.i)
+  for (k in 1:K){ # Calculate the PDF of each cluster for each data point
+    for (i in 1:N){
+      pdf.w[i,k] <- log(pi.c[k]) + fpr(theta=theta[k,], X[[i]])
+    }
+  }
   post.resp   <-  pdf.w - apply(pdf.w, 1, logSumExp) # Normalize the log probability
-  post.resp   <- apply(post.resp, 2, exp) # Eponentiate to get actual probabilities
+  post.resp   <- apply(post.resp, 2, exp) # Exponentiate to get actual probabilities
   
   ## M-step
   for (k in 1:K){
     # Update mixing proportions
     pi.c[k]   <- sum(post.resp[, k] / sum(post.resp))
     
-    p.optim <- optim(par=c(.6,.8,.2), fn=fpr, gr=gpr, n.i, method="CG")
+    theta.optim <- optim(par=theta[k,], fn=fpr, gr=gpr, X[[i]], method="CG")
+  }
 }
 
-
-
-
-t <- c(10, 11, 10, 11, 10)
-m <- c(5, 7, 6, 3, 5)
-x <- c(.7, .1, .2, .3, .8)
-n.i <- list(m=m, t=t)
-ss <- optim(par=c(.6,.8,.2), fn=fpr, gr=gpr, n.i, method="CG")
