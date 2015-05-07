030f575985467281044dae69f7251049f3d4e249
andreaskapou <kapouranis.andreas@gmail.com>
2015-05-07 18:14:33 +0100

    Implement M-step for EM, but it seems that there is a bug..

--- Expectation-Maximization/em-probit-reg.R
+++ Expectation-Maximization/em-probit-reg.R
@@ -7,6 +7,8 @@ cur.dir <- dirname(parent.frame(2)$ofile)
 setwd(cur.dir)
 library(R.utils)
 sourceDirectory("../lib", modifiedOnly=FALSE) # Source the 'lib' directory
+set.seed(1)
+
 
 ff <- function(theta, X){
   g   <- theta[1]*X^2 + theta[2]*X + theta[3]
@@ -15,10 +17,26 @@ ff <- function(theta, X){
 }
 
 
+l.theta <- function(theta, D, post.resp){
+  res <- 0
+  for (i in 1:length(D)){
+    res = res + fpr(theta, D[[i]]) * post.resp[i]
+  }
+  return(res)
+}
+
+dl.theta <- function(theta, D, post.resp){
+  res <- c(0,0,0)
+  for (i in 1:length(D)){
+    res = res + gpr(theta, D[[i]]) * post.resp[i]
+  }
+  return(res)
+}
+
 fpr <- function(theta, D){
-  X   <- D[1]
-  t   <- D[2]
-  m   <- D[3]
+  X   <- D[1,]
+  t   <- D[2,]
+  m   <- D[3,]
   g   <- theta[1]*X^2 + theta[2]*X + theta[3]
   Phi <- pnorm(g)
   res <- sum(-dbinom(x=m, size=t, prob=Phi, log=TRUE))
@@ -26,9 +44,9 @@ fpr <- function(theta, D){
 }
 
 gpr <- function(theta, D){
-  X   <- D[1]
-  t   <- D[2]
-  m   <- D[3]
+  X   <- D[1,]
+  t   <- D[2,]
+  m   <- D[3,]
   g   <- theta[1]*X^2 + theta[2]*X + theta[3]
   Phi <- pnorm(g)
   N   <- dnorm(g)
@@ -40,53 +58,54 @@ gpr <- function(theta, D){
   c(da, db, dc)
 }
 
-
-#t <- c(10, 11, 11, 11, 10, 10)
-#m <- c(8, 7, 7, 3, 2, 2)
-#X <- c(-50, -40, -30, 20, 30, 50)/(50/2)
-#ss <- optim(par=c(0,0,0), fn=fpr, gr=gpr, X, t, m, method="CG")
-
-
-# plot the function
-#xs <- seq(-1,1,len=100) # create some values
-#plot(x=xs, y=ff(theta=ss$par, xs), type="l", xlab="x", ylab="", 
-#     main=expression(a*x^2 + b*x + c))
-
 ##====================
 # Generate the data  #
 ##====================
-epsilon <- 0.00001    # Convergence paramater
+epsilon <- 1e-04      # Convergence paramater
 N       <- 300        # Total number of objects to create
 K       <- 3          # Number of clusters
-pi.c    <- c(0.45, 0.35, 0.2) # Mixing proportions
+pi.c    <- c(0.4, 0.3, 0.3) # Mixing proportions
 
-theta   <- matrix(0, nrow=K, ncol=3)
-for (i in 1:K){
-  theta[i,] <- c(0+i/100, 0+i/100, 0+i/100)
+theta   <- matrix(0, nrow=3, ncol=K)
+for (k in 1:K){
+  theta[,k] <- c(0+k/100, 0+k/100, 0+k/100)
 }
 
-X       <- gen.meth.data(N=N, pi.c=pi.c) # Generate methylation profiles data
+theta[,1] <- c(-0.024, -4, -1.01)
+theta[,2] <- c(0.024, 4, -1.01)
+theta[,3] <- c(-4, 0, 0.4)
+
+X       <- gen.meth.data2(N=N, pi.c=pi.c) # Generate methylation profiles data
 
 pdf.w       <- matrix(, N, K) # Hold the PDF of each point on each cluster k
 post.resp   <- matrix(, N, K)
 log.likel   <- 0
 
-for (t in 1:1000){
+for (t in 1:10){
   ## E-step
   for (k in 1:K){ # Calculate the PDF of each cluster for each data point
     for (i in 1:N){
-      pdf.w[i,k] <- log(pi.c[k]) + fpr(theta=theta[k,], X[[i]])
+      pdf.w[i,k] <- log(pi.c[k]) + fpr(theta=theta[,k], X[[i]])
     }
   }
-  post.resp   <-  pdf.w - apply(pdf.w, 1, logSumExp) # Normalize the log probability
+  
+  post.resp   <- pdf.w - apply(pdf.w, 1, logSumExp) # Normalize the log probability
   post.resp   <- apply(post.resp, 2, exp) # Exponentiate to get actual probabilities
   
   ## M-step
   for (k in 1:K){
     # Update mixing proportions
-    pi.c[k]   <- sum(post.resp[, k] / sum(post.resp))
+    pi.c[k]   <- sum(post.resp[,k] / sum(post.resp))
     
-    theta.optim <- optim(par=theta[k,], fn=fpr, gr=gpr, X[[i]], method="CG")
+    opt.theta <- optim(par=theta[,k], fn=l.theta, gr=dl.theta, X, post.resp[,k], method="CG", 
+                                                                    control = list(maxit = 10))
+    theta[,k] <- opt.theta$par
   }
 }
 
+#theta[,1] <- c(-0.024, -4, -1.01)
+#theta[,1] <- c(-4, 0, 0.4)
+#theta[,1] <- c(0.024, 4, -1.01)
+xs <- seq(-1,1,len=100) # create some values
+plot(x=xs, y=ff(theta=theta[,1], xs), type="l", xlab="x", ylab="", 
+     main=expression(a*x^2 + b*x + c))
