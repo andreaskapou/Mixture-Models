604fe7a9cad8124077d63ac484e856f7a4149d46
andreaskapou <kapouranis.andreas@gmail.com>
2015-06-01 20:03:08 +0100

    Likelihood and first derivatives wrt to parameters for Binomial Probit Regression using 3rd degree polynomials

---
+++ lib/sumGenBinomProbRegrLik.R
@@ -0,0 +1,31 @@
+##=======================================================================
+# sumBinomProbRegrLik - Sum of Binomial distributed Probit Regression   #
+# Likelihood over all the dataset D, weighted by the corresponding      #
+# responsibilites of each cluster. This function will be used in an     #
+# Optimization procedure for the M-step of the EM algorithm             #
+#                                                                       #
+# Usage: res  <- sumBinomProbRegrLik(theta, D, post.resp)               #
+#                                                                       #
+# Input:                                                                #
+#     theta   is a vector of the parameters of the function             #
+#     D       is a list, where each entry contains a 3 by L dimensional #
+#             matrix containing:                                        #
+#         X.i is a vector (of length L) with the corresponding values   #
+#               at each position for region i                           #
+#         t.i is a vector (of length L) with the number of trials, in   #
+#               the context of Binomal distributed data for region i    #
+#         m.i is a vector (of length L) with the number of successes in #
+#               the t trials for region i                               #
+#     post.resp are the posterior probabilities, rersponsibility the    #
+#               cluster k takes on data point x                         #
+# Output:                                                               #
+#     res     is the total log likelihood for all the regions i         #
+#                                                                       #
+##=======================================================================
+sumGenBinomProbRegrLik <- function(theta, D, post.resp){
+  res <- 0
+  for (i in 1:length(D)){
+    res = res + genBinomProbRegrLik(theta, D[[i]], mode=1) * post.resp[i]
+  }
+  return(res)
+}
