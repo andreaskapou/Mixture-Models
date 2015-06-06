a7189382a6682d0a2893b3a26de41a419ebcd612
andreaskapou <kapouranis.andreas@gmail.com>
2015-06-06 16:21:25 +0100

    Function for computing the EM algorith for PMMs

--- Expectation-Maximization/pmm-EM.R
+++ Expectation-Maximization/pmm-EM.R
@@ -1,87 +1,97 @@
-##===============================
-# Set the working directory and #
-# load any required libraries   #
-##===============================
-cur.dir <- dirname(parent.frame(2)$ofile)
-setwd(cur.dir)
-library(R.utils)
-sourceDirectory("../lib", modifiedOnly=FALSE) # Source the 'lib' directory
+#' Performs EM algorithm for Poisson Mixture Models (GMMs).
+#' 
+#' Notation and algorithm follows Bishop's book Ch.9, "Pattern Recognition 
+#' and Machina Learning". BUT, it computes the Negative Log Likelihood (NLL):
+#' - ln p(X|l,p) = - Sum_{N}(ln(Sum_{K}(p_k * Poi(x_n|l))))
+#' 
+#' This method works in different modes, depending on the parameters given. 
+#' If no 'theta' parameter is given, then it initializes the parameters using
+#' 'kmeans' algorithm. 
 
-##====================
-# Generate the data  #
-##====================
-epsilon <- 1e-06    # Convergence paramater
-K       <- 3        # Number of clusters
-X       <- gen.poisson(K=K, pi.c=c(.3,.2,.5), lambdas=c(5,20,50))
-
-##=========================
-# Initialize variables    #
-##=========================
-N       <- length(X)                  # Length of the dataset
-cl      <- kmeans(X, K, nstart = 25)  # Use Kmeans with random starts
-C.n     <- cl$cluster                 # get the mixture components
-pi.c    <- as.vector(table(C.n)/length(X)) # mixing proportions
-lambdas <- as.vector(cl$centers)      # means for each Gaussian
-
-isLog       <- TRUE 
-post.resp   <- matrix(, N, K)         # Hold responsibilities
-pdf.w       <- matrix(, N, K)         # Hold PDF of each point on each cluster k
-logLik      <- 0                      # Initialize log likelihood
-
-##===============================
-# Run Expectation Maximization  #
-##===============================
-for (i in 1:1000){                    # Loop until convergence
-  prevLogLik  <- logLik               # Store to check for convergence
+pmm.EM <- function(X, K=2, theta, epsilon=1e-10, maxIter=1000, isLog=TRUE, isDebug=FALSE){
   
-  ##========
-  # E-Step #
-  ##========
-  if (!isLog){
-    for (k in 1:K){ # Calculate the weighted PDF of each cluster for each data point
-      pdf.w[,k] <- pi.c[k] * dpois(X, lambda=lambdas[k])
-    }
-    post.resp   <- pdf.w / rowSums(pdf.w)             # Get responsibilites by normalization
-    
-    # ln p(X|mu,S,p) = Sum_{n=1}^{N}(ln(Sum_{k=1}^{K}(p_k * N(x_n|mu_k, S_k))))
-    logLik   <- sum(log(colSums(pdf.w)))              # Evaluate the log likelihood
+  N         <- length(X)                        # Length of the dataset
+  post.resp <- matrix(0, nrow=N, ncol=K)        # Hold responsibilities
+  pdf.w     <- matrix(0, nrow=N, ncol=K)        # Hold weighted PDFs
+  all.NLL   <- vector(mode="numeric")           # Hold NLL for all EM iterations
+  NLL       <- 1e+40                            # Initialize Negative Log Likelihood
+  
+  # If 'theta' parameter is empty, we initialize parameters using 'kmeans'
+  if (missing(theta)){
+    cl      <- kmeans(X, K, nstart = 25)        # Use Kmeans with random starts
+    C.n     <- cl$cluster                       # Get the mixture components
+    lambdas <- as.vector(cl$centers)            # Mean and variance for each cluster
+    pi.c    <- as.vector(table(C.n)/NROW(X))    # Mixing proportions
   }else{
-    for (k in 1:K){
-      pdf.w[,k] <- log(pi.c[k]) + dpois(X, lambda=lambdas[k], log=TRUE)
-    }
-    post.resp   <- pdf.w - apply(pdf.w, 1, logSumExp) # Normalize the log probability
-    post.resp   <- apply(post.resp, 2, exp)           # Exponentiate to get actual probabilities
+    lambdas <- theta$lambdas                    # Mean and variance for each cluster
+    pi.c    <- theta$pi.c                       # Mixing proportions
+  }
+
+  if (isDebug){
+    cat("Initial values:\n")
+    cat("Lambdas:", lambdas, "\n")
+    cat("Mixing proportions:", pi.c, "\n")
     
-    logLik   <- sum(log(colSums(exp(pdf.w))))         # Evaluate the log likelihood
+    cat("Initial NLL:", NLL, "\n")
   }
   
-  ##========
-  # M-Step #
-  ##========
-  for (k in 1:K){
-    N.k         <- sum(post.resp[,k])                 # Sum of responsibilities for cluster k
-    pi.c[k]     <- N.k / N                            # Update mixing proportions for cluster k
-    lambdas[k]  <- (post.resp[,k] %*% X) / N.k        # Update mean for cluster k
+  ##=========================================
+  # Run Expectation Maximization  algorithm #
+  ##=========================================
+  for (i in 1:maxIter){                         # Loop until convergence
+    prevNLL  <- NLL                             # Store NLL to check for convergence
+    
+    ##===================
+    #       E-Step      #
+    ##===================
+    if (!isLog){
+      # Calculate weighted PDF of each cluster for each data point
+      for (k in 1:K){
+        pdf.w[,k] <- pi.c[k] * dpois(X, lambda=lambdas[k])
+      }
+      Z           <- rowSums(pdf.w)             # Normalization constant
+      post.resp   <- pdf.w / Z                  # Get responsibilites by normalization
+      NLL         <- -sum(log(Z))               # Evaluate the NLL
+    }else{
+      for (k in 1:K){
+        pdf.w[,k] <- log(pi.c[k]) + dpois(X, lambda=lambdas[k], log=TRUE)
+      }
+      # Calculate probabilities using the logSumExp trick for numerical stability
+      Z           <- apply(pdf.w, 1, logSumExp)
+      post.resp   <- pdf.w - Z
+      post.resp   <- apply(post.resp, 2, exp)   # Exponentiate to get actual probabilities
+      NLL         <- -sum(Z)                    # Evaluate the NLL
+    }
+    
+    ##===================
+    #       M-Step      #
+    ##===================
+    N.k     <- colSums(post.resp)               # Sum of responsibilities for each cluster
+    pi.c    <- N.k/N                            # Update mixing proportions for each cluster
+    lambdas <- (t(X)%*%post.resp) / N.k          # Update mean and variance for each cluster
+    
+    
+    if (isDebug){
+      cat("i:", i, "\n")
+      cat("NLL:", NLL, "\n")
+    }
+    
+    NLL.Diff  <- prevNLL - NLL                  # Compute NLL difference after ith iteration
+    if (NLL.Diff < 0){
+      stop("Negative log likelihood increases - Something is wrong!")
+    }
+    
+    all.NLL   <- c(all.NLL, NLL)                # Keep all NLL in a vector  
+    if (NLL.Diff < epsilon){                    # Check for convergence.
+      break
+    }
+    
+  } #End of Expectation Maximization loop.
+  
+  message("Total iterations: ", i, "\n")
+  if (i == maxIter){
+    message("Warning: EM did not converge with given maximum iterations!\n\n")
   }
   
-  if (abs(logLik-prevLogLik) < epsilon){              # Check for convergence.
-    break
-  }
-  print(logLik)
-} #End of Expectation Maximization loop.
-
-##=====================================
-# Plot the data points and their pdfs #
-##=====================================
-# Create x points from min(X) to max(X)
-x <- seq(from = min(X)-1, to = max(X)+1, by = 1)
-hist(X, breaks = 22, freq=FALSE, col="lightblue", xlim=c(min(X)-1,max(X)+1),
-     ylim = c(0, .15), main=paste("Density plot of",K,"Poissons"), xlab = "x")
-
-mixture = 0.0
-for (k in 1:K){
-  # Calculate the estimated density
-  density <- dpois(x, lambda=lambdas[k])
-  mixture <- mixture + density * pi.c[k]
+  return(list(lambdas=lambdas, pi.c=pi.c, NLL=NLL, post.resp=post.resp, all.NLL=all.NLL))
 }
-lines(x, mixture, col="red",lwd=2)
