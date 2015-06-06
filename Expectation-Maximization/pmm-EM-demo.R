fa0349bcf73a1a78767910bea25f7d7ed637feae
andreaskapou <kapouranis.andreas@gmail.com>
2015-06-06 16:21:41 +0100

    Script for running EM-PMM algorithm

---
+++ Expectation-Maximization/pmm-EM-demo.R
@@ -0,0 +1,64 @@
+##=========================================================
+# Script for running Poisson Mixture Models using EM to   #
+# fit the model to the data.                              #
+##=========================================================
+
+##=============================================================
+# Set the working directory and load any required libraries   #
+##=============================================================
+cur.dir <- dirname(parent.frame(2)$ofile)
+setwd(cur.dir)
+source("pmm-EM.R")
+library(R.utils)
+sourceDirectory("../lib", modifiedOnly=FALSE) # Source the 'lib' directory
+set.seed(12345)         # Set seed for reproducible results
+
+##=============================================
+# Generate the data  and initialize variables #
+##=============================================
+N       <- 500          # Number of data points
+K       <- 3            # Number of clusters
+X       <- gen.poisson(K=K, pi.c=c(.3,.2,.5), lambdas=c(7,15,28))
+
+epsilon <- 1e-10        # Convergence paramater for EM
+maxIter <- 1000         # Maximum number of iterations for EM
+
+# Optional parameter initialization.
+lambdas <- c(1,2,3)     # Mean and variance of each cluster
+pi.c    <- rep(1/K, K)  # Initialize mixing proportions for each cluster
+
+theta   <- list(lambdas=lambdas, pi.c=pi.c) # Wrap all the parameters in a list
+
+##===================================================
+# Run PMM-EM, explicitly giving initial parameters  #
+##===================================================
+fit.pmm <- pmm.EM(X=X,
+                  K=K, 
+                  theta=theta, 
+                  epsilon=epsilon, 
+                  maxIter=maxIter, 
+                  isLog=TRUE, 
+                  isDebug=FALSE)
+
+##===================================================
+# Run PMM-EM, without initial parameters, then the  #
+# method will initilize parameters using k-means    #
+##===================================================
+fit.pmm.kmeans <- pmm.EM(X=X, K=K)
+
+
+##=====================================
+# Plot the data points and their pdfs #
+##=====================================
+# Create x points from min(X) to max(X)
+x <- seq(from = min(X)-1, to = max(X)+1, by = 1)
+hist(X, breaks = 22, freq=FALSE, col="lightblue", xlim=c(min(X)-1,max(X)+1),
+     ylim = c(0, .15), main=paste("Density plot of",K,"Poissons"), xlab = "x")
+
+mixture = 0.0
+for (k in 1:K){
+  # Calculate the estimated density
+  density <- dpois(x, lambda=fit.pmm$lambdas[k])
+  mixture <- mixture + density * fit.pmm$pi.c[k]
+}
+lines(x, mixture, col="red",lwd=2)
