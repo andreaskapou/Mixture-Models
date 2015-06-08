a381bf471cf60897a49c99bb0930c6da99b0561a
andreaskapou <kapouranis.andreas@gmail.com>
2015-06-08 21:28:40 +0100

    Impose ordering constraint so as to solve identifiability issue and get better estimates

--- Gibbs-Sampling/gmm1D-gibbs.R
+++ Gibbs-Sampling/gmm1D-gibbs.R
@@ -1,4 +1,4 @@
-gmm1D.gibbs <-  function(X, K=2, N.Sims, burnin, Normal, pi.cur, dir.a, logl=TRUE){
+gmm1D.gibbs <-  function(X, K=2, N.Sims=10000, burnin=5000, params, logl=TRUE){
   ##===================================================================
   # Function which uses Gibbs sampling so as to find the posterior    #
   # of the Hierarchical Dirichlet Finite Mixture Model with           #
@@ -9,15 +9,48 @@ gmm1D.gibbs <-  function(X, K=2, N.Sims, burnin, Normal, pi.cur, dir.a, logl=TRU
   post.resp     <- matrix(0, nrow=N, ncol=K)  # Posterior responsibilities
   pdf.w         <- matrix(0, nrow=N, ncol=K)  # PDF of each point on each cluster k
   C.n           <- matrix(0, nrow=N, ncol=K)  # Mixture components
-  x.k.bar       <- vector(length=K) # Sample mean for each cluster k 
-  ssd.k         <- vector(length=K) # Sum of square differences on each cluster k
+  C.matrix      <- matrix(0, nrow=N, ncol=K)  # Total Mixture components
+  NLL           <- vector(mode="numeric")     # Hold NLL for all MCMC iterations
+  x.k.bar       <- vector(length=K)           # Sample mean for each cluster k 
+  ssd.k         <- vector(length=K)           # Sum of square differences on each cluster k
   mu.draws      <- matrix(0, nrow=N.Sims-burnin, ncol=K) # Mean of each Gaussian
   tau.draws     <- matrix(0, nrow=N.Sims-burnin, ncol=K) # Precision of each Gaussian
   pi.draws      <- matrix(0, nrow=N.Sims-burnin, ncol=K) # Mixing Proportions
+  
+  ##===============================================
+  # If 'Normal' not defined initialize parameters #
+  ##===============================================
+  if (missing(params)){
+    Normal          <- list()                       # Create Normal object
+    cl              <- kmeans(X, K, nstart = 25)    # Use Kmeans with random starts
+    C               <- cl$cluster                   # Get the mixture components
+    pi.cur          <- as.vector(table(C)/NROW(X))  # Mixing proportions
+    dir.a           <- rep(1/K, K)                  # Dirichlet concentration parameter
+    Normal$mu       <- as.vector(cl$centers)        # Normal mean for each cluster
+    for (k in 1:K){
+      Normal$Tau[k] <- 1/var(X[C==k])               # Normal precision for each cluster
+    }
+    Normal$Norm     <- list(mu.0=mean(X), tau.0=1/sd(X))  # Normal hyperparameters
+    Normal$Gamma    <- list(shape.0=1, rate.0=1)          # Gamma hyperparameters
+    
+    ##=============================================================
+    # Ordering constraint:                                        #
+    #   Order all the values according to the mean parameter 'mu' #
+    ##=============================================================
+    Order       <- order(Normal$mu)
+    pi.cur      <- pi.cur[Order]
+    Normal$mu   <- Normal$mu[Order]
+    Normal$Tau  <- Normal$Tau[Order]
+  } else{
+    Normal          <- params$Normal
+    pi.cur          <- params$pi.cur
+    dir.a           <- params$dir.a
+  }
 
   for (t in 1:N.Sims){  # Start Gibbs sampling
     # Compute responsibilities
-    post.resp   <- compute.resp(X, pdf.w, K, Normal, pi.cur, logl) 
+    res         <- compute.resp(X, pdf.w, K, Normal, pi.cur, logl)
+    post.resp   <- res$post.resp
     # Draw mixture components for ith simulation
     C.n         <- c.n.update(N, K, post.resp)
     # Calculate component counts of each cluster
@@ -38,57 +71,109 @@ gmm1D.gibbs <-  function(X, K=2, N.Sims, burnin, Normal, pi.cur, dir.a, logl=TRU
     # Update posterior mean
     Normal$mu  <- mu.update(K, Normal, N.k, x.k.bar)
     
+    ##=============================================================
+    # Ordering constraint:                                        #
+    #   Order all the values according to the mean parameter 'mu' #
+    ##=============================================================
+    Order       <- order(Normal$mu)
+    pi.cur      <- pi.cur[Order]
+    Normal$mu   <- Normal$mu[Order]
+    Normal$Tau  <- Normal$Tau[Order]
+    
     # Keep only the simulations after the burn-in period has passed
     if (t > burnin){
+      NLL                       <- c(NLL, res$NLL)
+      C.matrix                  <- C.matrix + C.n
       pi.draws[t - burnin,]     <- pi.cur
       mu.draws[t - burnin,]     <- Normal$mu
       tau.draws[t - burnin,]    <- Normal$Tau
     }
   }
-  return(list(pi.draws=pi.draws, mu.draws=mu.draws, tau.draws=tau.draws))
-}
   
+  # Object to keep input data
+  dat         <- NULL
+  dat$X       <- X
+  dat$K       <- K
+  dat$N       <- N
+  dat$N.Sims  <- N.Sims
+  dat$burnin  <- burnin
+
+  # Object to hold all the MCMC draws
+  draws       <- NULL
+  draws$pi    <- pi.draws
+  draws$mu    <- mu.draws
+  draws$tau   <- tau.draws
+  
+  # Object to hold the summaries for the parameters
+  summary     <- NULL
+  summary$pi  <- apply(pi.draws, 2, mean)   # Expected value of mix. prop.
+  summary$mu  <- apply(mu.draws, 2, mean)   # Expected value of mean
+  summary$tau <- apply(tau.draws, 2, mean)  # Expected value of variance
+  summary$C   <- C.matrix / (N.Sims-burnin) # Convert C.matrix to probs
+  summary$NLL <- NLL
+  
+  # Object to hold the credible intervals for the parameters
+  cred.interv     <- NULL
+  cred.interv$pi  <- apply(pi.draws, 2, quantile, prob=c(0.025, 0.5, 0.975))
+  cred.interv$mu  <- apply(mu.draws, 2, quantile, prob=c(0.025, 0.5, 0.975))
+  cred.interv$tau <- apply(tau.draws, 2, quantile, prob=c(0.025, 0.5, 0.975))
+  
+  return(list(dat=dat, draws=draws, summary=summary, cred.interv=cred.interv))
+}
+
+
 # Compute the responsibilities
 compute.resp <- function(X, pdf.w, K, Normal, pi.cur, logl){
   if (logl){
-    for (k in 1:K) # Calculate the PDF of each cluster for each data point
+    for (k in 1:K){ # Calculate the PDF of each cluster for each data point
       pdf.w[,k] <- log(pi.cur[k]) + dnorm(X, mean=Normal$mu[k], 
                                           sd=1/sqrt(Normal$Tau[k]), log=TRUE)
-    post.resp   <-  pdf.w - apply(pdf.w,1,logSumExp) # Normalize the log probability
-    post.resp   <- apply(post.resp, 2, exp) # Eponentiate to get actual probabilities
+    }
+    # Calculate probabilities using the logSumExp trick for numerical stability
+    Z           <- apply(pdf.w, 1, logSumExp)
+    post.resp   <- pdf.w - Z
+    post.resp   <- apply(post.resp, 2, exp)   # Exponentiate to get actual probabilities
+    NLL         <- -sum(Z)                    # Evaluate the NLL
   }else{
-    for (k in 1:K) # Calculate the PDF of each cluster for each data point
+    for (k in 1:K){ # Calculate the PDF of each cluster for each data point
       pdf.w[,k] <- pi.cur[k] * dnorm(X, mean=Normal$mu[k], sd=1/sqrt(Normal$Tau[k]))
-    post.resp   <- pdf.w / rowSums(pdf.w) # Get responsibilites by normalizarion
+    }
+    Z           <- rowSums(pdf.w)             # Normalization constant
+    post.resp   <- pdf.w / Z                  # Get responsibilites by normalization
+    NLL         <- -sum(log(Z))               # Evaluate the NLL
   }
-  return(post.resp)
+  return(list(post.resp=post.resp, NLL=NLL))
 }
+
 # Update the mixture components 
 c.n.update <- function(N, K, post.resp){
   c.i.draw <- matrix(0, nrow=N, ncol=K)
   for (i in 1:N){ # Sample one point from a multinomial i.e. ~ Discrete
-    c.i.draw[i,] = rmultinom(1, 1, post.resp[i,])
+    c.i.draw[i,] = rmultinom(n=1, size=1, post.resp[i,])
     #c.i.draw[i] <- which(as.vector(rmultinom(1,1,post.resp[i,])) == TRUE)
   }
   return(c.i.draw)
 }
+
 # Update the mixing proportions
 pi.update <- function(dir.a, N.k){
   a.n <- dir.a + N.k
-  return(as.vector(rdirichlet(n=1, alpha=a.n))) # Sample from Dirichlet
+  return(as.vector(MCMCpack::rdirichlet(n=1, alpha=a.n))) # Sample from Dirichlet
 }
+
 # Update the posterior precision
 tau.update <- function(K, Normal, N.k, x.k.bar, ssd.k){
   tau.posterior <- vector(length=K)
   for (k in 1:K){
-    alpha.n <- Normal$Gamma$a + N.k[k]/2
-    beta.n  <- Normal$Gamma$b + ssd.k[k]/2 + ( 
+    alpha.n <- Normal$Gamma$shape.0 + N.k[k]/2
+    beta.n  <- Normal$Gamma$rate.0 + ssd.k[k]/2 + ( 
           (N.k[k] * Normal$Norm$tau.0 * ((x.k.bar[k] - Normal$Norm$mu.0)^2)) / 
                                            (2 * (N.k[k] + Normal$Norm$tau.0)) )
-    tau.posterior[k] <- rgamma(1, alpha.n, beta.n) # Sample from Gamma
+    tau.posterior[k] <- rgamma(1, shape=alpha.n, rate=beta.n) # Sample from Gamma
   }
   return(tau.posterior)
 }
+
 # Update the posterior mean
 mu.update <- function(K, Normal, N.k, x.k.bar){
   mu.posterior <- vector(length=K)
